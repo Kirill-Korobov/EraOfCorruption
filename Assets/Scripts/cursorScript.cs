6fe19170bdc7e79d1a1ce93c32e43fd150bb3682
artemi4csharp <152061991+artemi4csharp@users.noreply.github.com>
2024-10-13 20:03:01 +0300

    Change cursor behaviour

--- Assets/Scripts/cursorScript.cs
+++ Assets/Scripts/cursorScript.cs
@@ -1,21 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class cursorScript : MonoBehaviour
 {
-    public Texture2D cursorTexture;
-    public Vector2 cursorPosition = Vector2.zero;
-    public CursorMode cursorMode = CursorMode.Auto;
+    private float idleTime = 5.0f;
+    private float lastMouseMoveTime;
+    private Vector3 lastMousePosition;
+    public bool isCursorVisible = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.SetCursor(cursorTexture, cursorPosition, cursorMode);
+        lastMousePosition = Input.mousePosition;
+        lastMouseMoveTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.mousePosition != lastMousePosition)
+        {
+            lastMouseMoveTime = Time.time;
+            lastMousePosition = Input.mousePosition;
+
+            if (!isCursorVisible)
+            {
+                ShowCursor();
+            }
+        }
+
+        if (Time.time - lastMouseMoveTime > idleTime)
+        {
+            if (isCursorVisible)
+            {
+                HideCursor();
+            }
+        }
+    }
+
+    void ShowCursor()
+    {
+        Cursor.visible = true;
+        isCursorVisible = true;
+    }
+
+    void HideCursor()
+    {
+        Debug.Log("Cursor hide");
+        Cursor.visible = false;
+        isCursorVisible = false;
     }
 }
