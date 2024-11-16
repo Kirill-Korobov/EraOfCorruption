78b32d7e1cc7b2fa8595f4d868e6f4f63bfe331b
kolibri300 <dusuk964@gmail.com>
2024-11-16 17:45:46 +0200

    Put assets in order

---
+++ Assets/Scripts/AllEffects.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static class AllEffects
+{
+    static void Slowness()
+    {
+
+    }
+    static void Cursed()
+    {
+
+    }
+
+}
