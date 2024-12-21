5b1470ea005acde197560551268f76be680f511a
artemi4csharp <152061991+artemi4csharp@users.noreply.github.com>
2024-12-21 17:07:31 +0200

    Testting enemy mechanics

--- Assets/Scripts/Classes/Player.cs
+++ Assets/Scripts/Classes/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed = 10f;
     public float rotationSpeed = 100f;
     public float verticalRotationSpeed = 100f;
     public float minXRotation = -45f;
@@ -13,12 +13,14 @@ public class Player : MonoBehaviour
     private float horizontalInput;
     private float verticalInput;
     private float currentXRotation = 0f;
+    [SerializeField] private Camera camera;
 
     public int health {  get; private set; }
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -30,15 +32,25 @@ public class Player : MonoBehaviour
         Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
         transform.Translate(movement * moveSpeed * Time.deltaTime, Space.Self);
 
-        float mouseX = Input.GetAxis("Mouse X");
-        transform.Rotate(Vector3.up, mouseX * rotationSpeed);
+        // float mouseX = Input.GetAxis("Mouse X");
+        // //camera.transform.Rotate(Vector3.up, mouseX * rotationSpeed);
+        // camera.transform.rotation = Quaternion.Euler(0, mouseX * rotationSpeed, 0) * camera.transform.rotation;
+        
 
-        float mouseY = Input.GetAxis("Mouse Y");
-        currentXRotation -= mouseY * verticalRotationSpeed;
+        // float mouseY = Input.GetAxis("Mouse Y");
+        // currentXRotation -= mouseY * verticalRotationSpeed;
+        // currentXRotation = Mathf.Clamp(currentXRotation, minXRotation, maxXRotation);
+        // camera.transform.rotation = Quaternion.Euler(currentXRotation, 0, 0);
+
+        currentXRotation -= Input.GetAxis("Mouse Y") * verticalRotationSpeed * Time.deltaTime;
         currentXRotation = Mathf.Clamp(currentXRotation, minXRotation, maxXRotation);
 
-        Vector3 clampedRotation = new Vector3(currentXRotation, transform.eulerAngles.y, transform.eulerAngles.z);
-        transform.eulerAngles = clampedRotation;
+        transform.rotation *= 
+        Quaternion.AngleAxis( Time.deltaTime * verticalRotationSpeed * Input.GetAxis("Mouse X"), Vector3.up );
+
+        transform.rotation *= 
+        Quaternion.Euler(currentXRotation, 0, 0);
+
     }
     public void TakeDamage(int damage)
     {
