5467fed4a8ef7458f32a043cb7803f7c6e051d58
Kirill-Korobov <k.korobov10@gmail.com>
2025-01-14 09:54:32 +0200

    Small changes in StartMenuOperator
    Added exit from menus in start menu using escape key.

--- Assets/Scripts/UIOperators/StartMenuOperator.cs
+++ Assets/Scripts/UIOperators/StartMenuOperator.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class StartMenuOperator : MonoBehaviour
 {
     [SerializeField] private Canvas settingsCanvas, achievementsCanvas, developersCanvas;
-    [SerializeField] private GameObject startMenuContent, chooseGameWindow;
+    [SerializeField] private GameObject startMenuContent, chooseGameWindow, createGameWindow, deleteGameWindow, bindWindow;
 
     private void Awake()
     {
@@ -12,6 +12,31 @@ public class StartMenuOperator : MonoBehaviour
         settingsCanvas.gameObject.SetActive(false);
         achievementsCanvas.gameObject.SetActive(false);
         developersCanvas.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            if (createGameWindow.activeSelf || deleteGameWindow.activeSelf)
+            {
+                createGameWindow.SetActive(false);
+                deleteGameWindow.SetActive(false);
+                chooseGameWindow.SetActive(true);
+            }
+            else if (bindWindow.activeSelf)
+            {
+                bindWindow.SetActive(false);
+            }
+            else if (chooseGameWindow.activeSelf || settingsCanvas.gameObject.activeSelf || achievementsCanvas.gameObject.activeSelf)
+            {
+                startMenuContent.SetActive(true);
+                chooseGameWindow.gameObject.SetActive(false);
+                settingsCanvas.gameObject.SetActive(false);
+                achievementsCanvas.gameObject.SetActive(false);
+                developersCanvas.gameObject.SetActive(false);
+            }
+        }
     }
 
     public void OpenGameChoice()
